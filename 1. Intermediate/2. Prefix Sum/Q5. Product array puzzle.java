06d53a318d506101d10d9ee87f8a4bf20cde9c9d
Mohnish <88628261+MohnishPurella@users.noreply.github.com>
2023-07-26 14:49:02 +0530

    Update Q5. Product array puzzle.java

--- 1. Intermediate/2. Prefix Sum/Q5. Product array puzzle.java
+++ 1. Intermediate/2. Prefix Sum/Q5. Product array puzzle.java
@@ -1,5 +1,20 @@
 public class Q5 {
     public static void main(String[] args){
-
+        int n= A.length;
+        int[] prefProd= new int[n];
+        prefProd[0]=1;
+        for(int i=1;i<n;i++){
+            prefProd[i]= prefProd[i-1]*A[i-1];
+        }
+        int [] suffProd= new int[n];
+        suffProd[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            suffProd[i]=suffProd[i+1]*A[i+1];
+        }
+        int []outArr= new int[n];
+        for(int i=0;i<n;i++){                      
+            outArr[i]=prefProd[i]*suffProd[i];
+        }
+        return outArr;
     }
 }
